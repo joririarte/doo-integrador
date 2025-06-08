7a571abe269ad676f35c0fffa99427467d268c48
Jor Iriarte <106839094+joririarte@users.noreply.github.com>
2025-06-08 14:46:55 -0300

    + Se implementa Builder en DetalleVenta y se añaden metodos de negocio

--- src/main/java/com/ventas/model/DetalleVenta.java
+++ src/main/java/com/ventas/model/DetalleVenta.java
@@ -1,15 +1,86 @@
 package com.ventas.model;
 
-public class DetalleVenta {
+import java.util.Arrays;
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.config.Configuration.AccessLevel;
+
+import com.ventas.dto.DetalleVentaDto;
+import com.ventas.factories.FabricaDao;
+
+public class DetalleVenta extends Modelo{
     private String nombre;
     private int cantidad;
     private float descuentoRecargo;
     private float precioVenta;
     private Producto producto;
 
+    //#region Constructors
+
     public DetalleVenta() {
+        this.dao = FabricaDao.fabricar("DetalleVentaDao");
+        this.mapper = new ModelMapper();
+        mapper.getConfiguration().setFieldMatchingEnabled(true).setFieldAccessLevel(AccessLevel.PRIVATE);
     }
 
+    public DetalleVenta(DetalleVentaBuilder builder) {
+        this();
+        this.setNombre(builder.nombre);
+        this.setCantidad(builder.cantidad);
+        this.setDescuentoRecargo(builder.descuentoRecargo);
+        this.setPrecioVenta(builder.precioVenta);
+        this.setProducto(builder.producto);
+    }
+    
+    //#endregion
+
+    //#region DetalleVentaBuilder
+
+    public static class DetalleVentaBuilder {
+        private String nombre;
+        private int cantidad;
+        private float descuentoRecargo;
+        private float precioVenta;
+        private Producto producto;
+
+        public static DetalleVentaBuilder getBuilder() {
+            return new DetalleVentaBuilder();
+        }
+
+        public DetalleVentaBuilder conNombre(String nombre) {
+            this.nombre = nombre;
+            return this;
+        }
+
+        public DetalleVentaBuilder conCantidad(int cantidad) {
+            this.cantidad = cantidad;
+            return this;
+        }
+
+        public DetalleVentaBuilder conDescuentoRecargo(float descuentoRecargo) {
+            this.descuentoRecargo = descuentoRecargo;
+            return this;
+        }
+
+        public DetalleVentaBuilder conPrecioVenta(float precioVenta) {
+            this.precioVenta = precioVenta;
+            return this;
+        }
+
+        public DetalleVentaBuilder conProducto(Producto producto) {
+            this.producto = producto;
+            return this;
+        }
+
+        public DetalleVenta build() {
+            return new DetalleVenta(this);
+        }
+    }
+
+    //#endregion
+
+    //#region Getters y Setters
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
@@ -50,5 +121,60 @@ public class DetalleVenta {
         return producto;
     }
 
-    public float calcularSubtotal() { return 0; }
+    //#endregion
+    
+    //#region Business Methods
+
+    public float calcularSubtotal() {
+        return this.cantidad * this.getPrecioVenta();
+    }
+    
+    public List<DetalleVenta> listarDetalleVentas() {
+        try {
+            List<DetalleVentaDto> listado = this.dao.listarTodos();
+            if (!listado.isEmpty()) {
+                List<DetalleVenta> listaDetalleVentas = Arrays.asList(this.mapper.map(listado, DetalleVenta[].class));
+                return listaDetalleVentas;
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    public List<DetalleVenta> consultarDetalleVenta(List<String> params) {
+        List<DetalleVenta> listadoDetalleVentas = null;
+        try {
+            DetalleVentaDto detalleVentaDto = this.mapper.map(this, DetalleVentaDto.class);
+            List<DetalleVentaDto> detalleVentasDto = this.dao.buscar(detalleVentaDto, params);
+            listadoDetalleVentas = Arrays.asList(this.mapper.map(detalleVentasDto, DetalleVenta[].class));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return listadoDetalleVentas;
+    }
+
+    public Boolean registrarDetalleVenta() {
+        try {
+            DetalleVentaDto detalleVentaDto = this.mapper.map(this, DetalleVentaDto.class);
+            detalleVentaDto = (DetalleVentaDto) this.dao.actualizar(detalleVentaDto, null);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public Boolean eliminarDetalleVenta() {
+        try {
+            DetalleVentaDto detalleVentaDto = this.mapper.map(this, DetalleVentaDto.class);
+            detalleVentaDto = (DetalleVentaDto) this.dao.borrar(detalleVentaDto);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    //#endregion
 }
