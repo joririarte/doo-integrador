03b24a8670819c32604b8a8494c5d5ab76f7a463
Jor Iriarte <106839094+joririarte@users.noreply.github.com>
2025-06-08 14:09:42 -0300

    + se agrega el patron builder a Venta

--- src/main/java/com/ventas/model/Venta.java
+++ src/main/java/com/ventas/model/Venta.java
@@ -1,9 +1,13 @@
 package com.ventas.model;
 
 import java.util.*;
-import java.util.Date;
 
-public class Venta {
+import org.modelmapper.ModelMapper;
+import org.modelmapper.config.Configuration.AccessLevel;
+
+import com.ventas.factories.FabricaDao;
+
+public class Venta extends Modelo {
     private Empleado vendedor;
     private Date fecha;
     private String estado;
@@ -12,8 +16,85 @@ public class Venta {
     private Cliente cliente;
     private List<DetalleVenta> detalleVenta;
 
+    //#region Constructors
     public Venta() {
+        this.dao = FabricaDao.fabricar("VentaDao");
+        this.mapper = new ModelMapper();
+        mapper.getConfiguration().setFieldMatchingEnabled(true).setFieldAccessLevel(AccessLevel.PRIVATE);
     }
+
+    public Venta(VentaBuilder builder) {
+        this(); // Llama al constructor por defecto
+        this.setVendedor(builder.vendedor);
+        this.setFecha(builder.fecha);
+        this.setEstado(builder.estado);
+        this.setMontoPagado(builder.montoPagado);
+        this.setMedioPago(builder.medioPago);
+        this.setCliente(builder.cliente);
+        this.setDetalleVenta(builder.detalleVenta);
+    }
+    //#endregion
+
+    //#region VentaBuilder
+
+    public static class VentaBuilder {
+    private Empleado vendedor;
+    private Date fecha;
+    private String estado;
+    private float montoPagado;
+    private MedioPago medioPago;
+    private Cliente cliente;
+    private List<DetalleVenta> detalleVenta;
+
+    public static VentaBuilder getBuilder() {
+        return new VentaBuilder();
+    }
+
+    public VentaBuilder conVendedor(Empleado vendedor) {
+        this.vendedor = vendedor;
+        return this;
+    }
+
+    public VentaBuilder conFecha(Date fecha) {
+        this.fecha = fecha;
+        return this;
+    }
+
+    public VentaBuilder conEstado(String estado) {
+        this.estado = estado;
+        return this;
+    }
+
+    public VentaBuilder conMontoPagado(float montoPagado) {
+        this.montoPagado = montoPagado;
+        return this;
+    }
+
+    public VentaBuilder conMedioPago(MedioPago medioPago) {
+        this.medioPago = medioPago;
+        return this;
+    }
+
+    public VentaBuilder conCliente(Cliente cliente) {
+        this.cliente = cliente;
+        return this;
+    }
+
+    public VentaBuilder conDetalleVenta(List<DetalleVenta> detalleVenta) {
+        this.detalleVenta = detalleVenta;
+        return this;
+    }
+
+    public Venta build() {
+        return new Venta(this);
+    }
+}
+
+
+    //#endregion
+
+    //#region Getters y Setters
+
     public Empleado getVendedor() {
         return vendedor;
     }
@@ -56,6 +137,11 @@ public class Venta {
     public void setDetalleVenta(List<DetalleVenta> detalleVenta) {
         this.detalleVenta = detalleVenta;
     }
+
+    //#endregion
+
+    //#region Business Methods
+    
     public float calcularMontoTotal() { return 0; }
     public float calcularVuelto() { return 0; }
     public void setCliente() {}
@@ -64,4 +150,6 @@ public class Venta {
     public void cobrar() {}
     public boolean checkCobro() { return false; }
     public void cancelar() {}
+    
+    //#endregion
 }
